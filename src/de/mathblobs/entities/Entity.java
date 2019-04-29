b6ec0047fb5f30ef785086d4d4aee2e72e7334d8
Mastermori <mastermoriii@gmail.com>
2019-04-29 16:34:36 +0200

    Added explaining comments

--- src/de/mathblobs/entities/Entity.java
+++ src/de/mathblobs/entities/Entity.java
@@ -5,16 +5,19 @@ import de.mathlib.Vector2;
 
 public abstract class Entity {
 
+    //---------- VARIABLES ----------
     protected static Main pa = Main.inst;
 
-    protected Vector2 pos;
-    protected Vector2 size;
+    protected Vector2 pos; //Position Vector
+    protected Vector2 size; //Dimensions Vector
 
-    protected Vector2 vel;
-    protected Vector2 acc;
+    protected Vector2 vel; //Velocity Vector
+    protected Vector2 acc; //Acceleration Vector
 
-    protected EntityHandler handler;
+    protected EntityHandler handler; //EntityHandler this is registered in
 
+
+    //---------- CONSTRUCTORS ----------
     public Entity(int x, int y, int width, int height, EntityHandler handler) {
         this(new Vector2(x, y), new Vector2(width, height), handler);
     }
@@ -23,43 +26,89 @@ public abstract class Entity {
         this.pos = pos;
         this.size = size;
         this.handler = handler;
-        handler.addEntity(this);
-        vel = new Vector2(0, 0);
-        acc = new Vector2(0, 0);
+        handler.addEntity(this); //Register this in the assigned handler
+        vel = new Vector2(0, 0); //Set the velocity to 0
+        acc = new Vector2(0, 0); //Set the acceleration to 0
     }
 
-    public void update(float delta){
-        vel = vel.add(acc.mult(delta));
-        pos = pos.add(vel.mult(delta));
+
+    //---------- UPDATE ----------
+    /**
+     * Update method called every frame - used to apply physics to the entity - override to add additional behaviour
+     * @param delta time used for calculation
+     */
+    public void update(float delta) {
+        vel = vel.add(acc.mult(delta)); //Update the position with the velocity
+        pos = pos.add(vel.mult(delta)); //Update the velocity with the acceleration
     }
 
-    public void draw(){
-        pa.fill(0);
-        pa.rect(pos.x, pos.y, size.x, size.y);
+
+    //---------- DRAW ----------
+    /**
+     * Draw method called every frame - override to draw entity
+     */
+    public abstract void draw();
+
+    /**
+     * Example draw method which can be used to draw an entity as a black square
+     */
+    public void Draw() {
+        pa.fill(0); //Make the fill color black
+        pa.rect(pos.x, pos.y, size.x, size.y); //Draw a rectangle at the entities position
     }
 
+
+    //---------- PHYSICS ----------
+    /**
+     * This method is called each time an object collides with this entity
+     * @param other Entity this collided with
+     */
     public abstract void collide(Entity other);
 
+    /**
+     * @return Vector2 with the left and right collision coordinates as x and y respectively
+     */
     public Vector2 getHorizontalColliders() {
         return new Vector2(pos.x, pos.x + size.x);
     }
 
+    /**
+     * @return Vector2 with the up and down collision coordinates as x and y respectively
+     */
     public Vector2 getVerticalColliders() {
         return new Vector2(pos.y, pos.y + size.y);
     }
 
+    /**
+     * Adds velocity to the Entity
+     * @param add velocity to add to this Entity
+     */
     public void addVel(Vector2 add) {
         vel = vel.add(add);
     }
 
+    /**
+     * Adds acceleration to the Entity
+     * @param add acceleration to add to this Entity
+     */
     public void addAcc(Vector2 add) {
         acc = acc.add(add);
     }
 
+
+    //---------- OTHER ----------
+    /**
+     * Destroy this Entity by releasing it's resources and removing it's reference from the handler
+     */
+    public void destroy() {
+        handler.removeEntity(this);
+    }
+
+
+    //---------- GETTERS/SETTERS ----------
     public Vector2 getPos() {
         return pos;
     }
-
     public void setPos(Vector2 pos) {
         this.pos = pos;
     }
@@ -67,12 +116,7 @@ public abstract class Entity {
     public Vector2 getSize() {
         return size;
     }
-
     public void setSize(Vector2 size) {
         this.size = size;
-    }
-
-    public void destroy() {
-        handler.removeEntity(this);
     }
 }
--- src/de/mathblobs/entities/EntityHandler.java
+++ src/de/mathblobs/entities/EntityHandler.java
@@ -3,25 +3,29 @@ package de.mathblobs.entities;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Used to handle multiple entities (running update and draw methods)
+ */
 public class EntityHandler {
-
-    /**
-     * Used to handle multiple entities (running update and draw methods)
-     */
 
     //---------- VARIABLES ----------
     private List<Entity> entities;
-    private List<Entity> rem;
-    private List<Entity> add;
+    private List<Entity> rem; //List to remove entities (cant remove in foreach) -> ConcurrentModificationException
+    private List<Entity> add; //List to add entities (cant add in foreach) -> ConcurrentModificationException
 
     //---------- CONSTRUCTORS ----------
     public EntityHandler() {
+        //Instantiate class variables (see above)
         entities = new LinkedList<>();
         rem = new LinkedList<>();
         add = new LinkedList<>();
     }
 
     //---------- UPDATE ----------
+    /**
+     * Update method called every frame - used to update Lists (add/rem) and call the entities update methods
+     * @param delta time used for calculation
+     */
     public void update(float delta) {
         updateLists();
 
@@ -29,16 +33,25 @@ public class EntityHandler {
     }
 
     //---------- DRAW ----------
+    /**
+     * Draw method called every frame - used call the entities draw methods
+     */
     public void draw() {
         entities.forEach((e) -> e.draw());
     }
 
     //---------- COLLISION CHECK ----------
+
+    /**
+     * Checks for collision between this and the provided handlers entities.
+     * Calls collision method on both entities colliding
+     * @param handler to check collisions with
+     */
     public void checkCollision(EntityHandler handler) {
         List<Entity> otherEntities = handler.entities;
         for (Entity e : entities) {
             for (Entity oe : otherEntities) {
-                if (checkEntityCollision(e, oe)) {
+                if (checkEntityCollision(e, oe)) { //if the entities are colliding, call both collide functions
                     e.collide(oe);
                     oe.collide(e);
                 }
@@ -46,6 +59,12 @@ public class EntityHandler {
         }
     }
 
+    /**
+     * Checks collision between to entities using their get..Colliders() methods
+     * @param e1
+     * @param e2
+     * @return true if the entities are colliding (intersecting)
+     */
     private boolean checkEntityCollision(Entity e1, Entity e2) {
         return e1.getHorizontalColliders().y > e2.getHorizontalColliders().x
                 && e1.getHorizontalColliders().x < e2.getHorizontalColliders().x
@@ -54,6 +73,9 @@ public class EntityHandler {
     }
 
     //---------- LIST MANAGEMENT METHODS ----------
+    /**
+     * Adds and removes entities from their respective lists and clears the add/rem lists
+     */
     private void updateLists() {
         if(!rem.isEmpty()) {
             rem.forEach((e) -> entities.remove(e));
@@ -65,9 +87,16 @@ public class EntityHandler {
         }
     }
 
+    /**
+     * Adds an entity to the handler (added next update call)
+     * @param entity
+     */
     public void addEntity(Entity entity) {
         add.add(entity);
     }
+    /**
+     * Removes an entity from the handler (removed next update call)
+     */
     public void removeEntity(Entity entity) {
         rem.add(entity);
     }
