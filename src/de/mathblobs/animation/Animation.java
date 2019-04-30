28bfb29a2fa1ee047a630e82c6ee09e142c74aaf
Mastermori <mastermoriii@gmail.com>
2019-04-30 18:23:21 +0200

    Added bare-bones Animation and MathTask class

---
+++ src/de/mathblobs/animation/Animation.java
@@ -0,0 +1,15 @@
+package de.mathblobs.animation;
+
+import processing.core.PImage;
+
+import java.util.List;
+
+public class Animation {
+
+    List<PImage> frames;
+
+    public Animation() {
+
+    }
+
+}
---
+++ src/de/mathblobs/tasks/MathTask.java
@@ -0,0 +1,12 @@
+package de.mathblobs.tasks;
+
+public abstract class MathTask {
+
+    String task;
+    int solution;
+
+    public MathTask() {
+
+    }
+
+}
